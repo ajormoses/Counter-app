35c3dc1dbba304063dd60acd511c64e2cd852343
ajormoses <ajormoses@gmail.com>
2022-11-07 00:04:12 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import Home from "./components/Home";
 import Nav from "./components/Nav";
 import CustomHookCounter from "./components/CustomHookCounter";
 import ReducerCounter from "./components/ReducerCounter";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/nav" exact element={<Nav />} />
-          <Route path="/hookcounter" exact element={<CustomHookCounter />} />
-          <Route path="/reducercounter" exact element={<ReducerCounter />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/nav" exact element={<Nav />} />
+        <Route path="/hookcounter" exact element={<CustomHookCounter />} />
+        <Route path="/reducercounter" exact element={<ReducerCounter />} />
+      </Routes>
     </div>
   );
 }
